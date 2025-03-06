acdfc4873033fdbe660195a778a201723ff51f36
Sebastian Wesołowski <Sebastian.Wesolowski@grupawp.pl>
2025-03-06 22:39:31 +0100

    Update struct error - fix short version

--- main.go
+++ main.go
@@ -40,15 +40,18 @@ func main() {
 	//fmt.Println(alex2)
 	//fmt.Println(alex3)
 
-	alexPointer := &alex //
-	alexPointer.updateName("Robocop")
+	//alexPointer := &alex
+	//alexPointer.updateName("Robocop")
+
+	alex.updateName("Robocop")
+
 	alex.print()
 	alex2.print()
 	alex3.print()
 }
 
-func (pointerToPerson *person) updateName(newFirstName string) {
-	(*pointerToPerson).firstName = newFirstName
+func (p *person) updateName(newFirstName string) {
+	(*p).firstName = newFirstName
 }
 
 func (p person) print() {
