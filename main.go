c0be3de7ef7043035803966bceaf14974a344f9c
Sebastian Wesołowski <Sebastian.Wesolowski@grupawp.pl>
2025-03-06 21:59:31 +0100

    Wielopoziomowe struct

--- main.go
+++ main.go
@@ -2,9 +2,15 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
@@ -12,10 +18,26 @@ func main() {
 	alex := person{
 		firstName: "Alex",
 		lastName:  "Anderson",
+		contact: contactInfo{
+			email:   "alex.anderson@gmail.com",
+			zipCode: 9400,
+		},
 	}
 
 	// SECOND WAY
-	//alex := person{"Alex", "Anderson"}
+	alex2 := person{"Robert", "Hawks", contactInfo{"seba.wesol@gmail.com", 9999}}
+
+	// THIRD WAY
+	var alex3 person
+
+	alex3.firstName = "Simon"
+	alex3.lastName = "Henderson"
+	alex3.contact.email = "tescior@gmail.com"
+	alex3.contact.zipCode = 222
 
 	fmt.Println(alex)
+	fmt.Println(alex2)
+	fmt.Println(alex3)
+
+	fmt.Printf("%+v\n", alex3)
 }
